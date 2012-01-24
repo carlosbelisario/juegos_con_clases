046e4b6a0530bed09469111ac3b2b04434a251a1
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2012-01-23 22:42:26 -0430

    guardando lo respectivo de usuarios en su carpeta 23-1-2012

---
+++ Usuarios/UsuariosInterface.php
@@ -0,0 +1,58 @@
+<?php
+namespace Usuarios;
+
+/**
+ *
+ * interface que gestiona los métodos de los usuarios
+ * @author Carlos Belisario <carlosbelisario.com>
+ */
+interface UsuariosManagerInterface 
+{
+    /**
+     * 
+     * @method registro
+     * metodo para registrar los usuarios
+     * @param Usuarios $usuario
+     */
+    public function registro(Usuarios $usuario);
+    
+    /**
+     * 
+     * @method editar
+     * metodo para editar usuario
+     * @param Usuarios $usuario
+     * @param String $usuario
+     */
+    public function editar(Usuario $usuario, $id);
+    
+    /**
+     * 
+     * @method borrar
+     * metodo para borrar usuario
+     * @param String $usuario
+     */
+    public function borrar($usuario); 
+    
+    /**
+     * 
+     * @method login 
+     * @param Usuario $usuario
+     */
+    public function login(Usuario $usuario);
+    
+    /**
+     * @method find
+     * metodo para buscar usuarios     * 
+     */
+    public function find();
+    
+    /**
+     * 
+     * @method findBy
+     * metodo para buscar a un usuario por un criterio
+     * @param Array $criterio
+     */
+    public function findBy(array $criterio);        
+    
+}
+?>
---
+++ Usuarios/UsuariosManager.php
@@ -0,0 +1,14 @@
+<?php
+namespace Usuarios;
+
+/**
+ * Clase para manegar a los Usuarios
+ *
+ * @author Carlos Belisario <carlosbelisario.com>
+ */
+class UsuariosManager implements UsuariosManagerInterface
+{
+    
+}
+
+?>
